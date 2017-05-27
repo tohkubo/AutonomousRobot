a9eca9598e0c41b3067acc6cd6f6f0ff5d9b298f
tohkubo <tohkubo@uci.edu>
2017-05-27 12:23:40 -0700

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,30 +1,20 @@
+import time
+from pynq.iop import Pmod_PWM
+from pynq.iop import PMODB
 from pynq import Overlay
-
 Overlay("base.bit").download()
 
-
-from pynq.iop import Pmod_PWM
-
-from pynq.iop import PMODB
-
-
-pwm = Pmod_PWM(PMODB, 0)
-
-
-import time
-
+signal = Pmod_PWM(PMODB, 0)
 
 # Generate a 10 us clocks with 50% duty cycle
+period = 20000
 
-period= 20000
+#6 is in the middle and 4 is 30 degree clockwise 8 is 30 degree counterclockwise
+duty = 4   
 
-duty= 4   #6 is in the middle and 4 is 30 degree clockwise 8 is 30 degree counterclockwise
-
-pwm.generate(period,duty)
-
+signal.generate(period,duty)
 
 # Sleep for 4 seconds and stop the timer
-
 time.sleep(4)
 
-pwm.stop()
+signal.stop()
