fabdf53bc98a607b62f8a523c66884c262c1c3b1
tohkubo <tohkubo@uci.edu>
2017-05-31 00:16:57 -0700

    Update main.py

--- MicroBlaze/main.py
+++ MicroBlaze/main.py
@@ -1,14 +1,12 @@
 # Make sure the base overlay is loaded
 from pynq import Overlay
 Overlay("base.bit").download()
-import time
-import numpy as np
-from MicroBlaze import MicroBlaze
+from UltraSonic import Sensor
     
-mb = MicroBlaze()
-values = mb.poll()
-filtered = mb.filt(values)
-mean = mb.calc(filtered)
+s = Sensor()
+values = s.poll()
+filtered = s.filt(values)
+mean = s.calc(filtered)
 print("Poll:\t\t" + ", ".join([str(v) for v in values]))
 print("Filtered:\t" + ", ".join([str(v) for v in filtered]))
 print("Avg:\t\t{}".format(mean))
