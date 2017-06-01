36deb7d2006785572b93cb3b3eeb3f4cb4e297d8
floydw1234 <floydw1234@gmail.com>
2017-05-31 23:53:49 -0700

    updated code fixed bugs

--- motors.py
+++ motors.py
@@ -1,3 +1,4 @@
+from pynq import Overlay
 Overlay("base.bit").download()
 
 from pynq.iop import Pmod_PWM
@@ -9,8 +10,7 @@ import time
 
 
 class Motor:
-    period= 20000
-    duty= 70
+
 
     def __init__(self):
         self.Cpin1 = Pmod_IO(PMODA,2,"out")
@@ -18,6 +18,8 @@ class Motor:
         self.Cpin3 = Pmod_IO(PMODA,6,"out")
         self.Cpin4 = Pmod_IO(PMODA,7,"out")
         self.pwm = Pmod_PWM(PMODB, 0)
+        self.period= 20000
+        self.duty= 70
 
     def turnLeft(self):
         self.Cpin1.write(0)
@@ -30,7 +32,7 @@ class Motor:
         self.Cpin3.write(1)
         self.Cpin4.write(0) ## turn left
         time.sleep(.1)
-        self.pwm1.generate(period,duty)
+        self.pwm.generate(self.period,self.duty)
         time.sleep(.4) #adjust this value to make sure that is turns exactly 90 degrees
         self.pwm.stop()
 
@@ -47,7 +49,7 @@ class Motor:
         self.Cpin3.write(0)
         self.Cpin4.write(1)
         time.sleep(.1)
-        self.pwm.generate(period,duty)
+        self.pwm.generate(self.period,self.duty)
         time.sleep(.4) #adjust this value to make sure that is turns exactly 90 degrees
         self.pwm.stop()
 
@@ -62,11 +64,11 @@ class Motor:
         self.Cpin3.write(1)
         self.Cpin4.write(0)
         time.sleep(.1)
-        self.pwm.generate(period,duty)
+        self.pwm.generate(self.period,self.duty)
         time.sleep(.9) #adjust this value to make sure that it goes exactly one space
         self.pwm.stop()
 
-    def goForward(self):
+    def goBackward(self):
         self.Cpin1.write(0)
         self.Cpin4.write(0)
         self.Cpin2.write(0)
@@ -77,6 +79,14 @@ class Motor:
         self.Cpin3.write(0)
         self.Cpin4.write(1)
         time.sleep(.1)
-        pwm1.generate(period,duty)
+        self.pwm.generate(self.period,self.duty)
         time.sleep(.9) #adjust this value to make sure that it goes exactly one space
         self.pwm.stop()
+m = Motor()
+m.turnRight()
+time.sleep(.5)
+m.turnLeft()
+time.sleep(.5)
+m.goForward()
+time.sleep(.5)
+m.goBackward()
