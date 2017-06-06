99c6a8342ab191bd6678af0c510fac1dfe9117eb
tohkubo <tohkubo@uci.edu>
2017-06-05 21:26:43 -0700

    (sometimes) working left turn and backtrack funcs;
    still needs to make sure that when turning left, it faces directly to the left by adjusting / straightening out first AND THEN turning left...same with uturn--needs to make sure the car is facing directly forward before doing a full 180 uturn.

--- Maze/Maze.py
+++ Maze/Maze.py
@@ -5,9 +5,9 @@ import time
 import numpy as np
 from pynq.iop import Arduino_Analog
 from pynq.iop import ARDUINO
-from pynq.iop import ARDUINO_GROVE_A1
-from pynq.iop import ARDUINO_GROVE_A4
-from pynq.iop import Arduino_IO
+# from pynq.iop import ARDUINO_GROVE_A1
+# from pynq.iop import ARDUINO_GROVE_A4
+# from pynq.iop import Arduino_IO
 from pynq.iop import Pmod_IO
 from pynq.iop import Pmod_PWM
 from pynq.iop import PMODA
@@ -53,7 +53,7 @@ class Motor:
         self.Cpin4.write(0) ## turn left
         time.sleep(.1)
         self.pwm.generate(self.period,self.duty)
-        time.sleep(.4)
+        time.sleep(.65)
         self.pwm.stop()
     
     #  off to the right, so straighten it by Left Adjust
@@ -85,7 +85,7 @@ class Motor:
         self.Cpin4.write(1)
         time.sleep(.1)
         self.pwm.generate(self.period,self.duty)
-        time.sleep(.4)
+        time.sleep(.55)
         self.pwm.stop()
 
     #  off to the right, so straighten it by Left Adjust
@@ -116,7 +116,7 @@ class Motor:
         self.Cpin4.write(0)
         time.sleep(.1)
         self.pwm.generate(self.period,self.duty)
-        time.sleep(.2)
+        time.sleep(.4)
         self.pwm.stop()
 
     def B(self):
@@ -132,6 +132,21 @@ class Motor:
         time.sleep(.1)
         self.pwm.generate(self.period,self.duty)
         time.sleep(.2)
+        self.pwm.stop()
+
+    def UTURN(self):
+        self.Cpin1.write(0)
+        self.Cpin2.write(0)
+        self.Cpin3.write(0)
+        self.Cpin4.write(0)
+        #initialized all pins
+        self.Cpin1.write(1)
+        self.Cpin2.write(0)
+        self.Cpin3.write(1)
+        self.Cpin4.write(0) ## turn left
+        time.sleep(.1)
+        self.pwm.generate(self.period,self.duty)
+        time.sleep(.95)
         self.pwm.stop()
 
 
@@ -177,7 +192,7 @@ class Maze:
             print('frontDist: ', frontDist)
             print('rightDist: ', rightDist)
             if frontDist != None:
-                if frontDist >= 30:
+                if frontDist >= 35:
                     if dist == 0:
                         dist = rightDist
                     self.path.append([x, y, 'F', rightDist])
@@ -197,64 +212,76 @@ class Maze:
 #                     self.map[x][y + 1] = self.counter
 #                     self.visited[x][y + 1] = True
 
-                    # self.search(x, y + 1, rightDist if len(self.path) >= 3 else self.path[-2][-1])
-                    self.search(x, y + 1, rightDist)
+                    self.search(x, y + 1, rightDist if len(self.path) <= 2 else self.path[-2][-1])
+#                     self.search(x, y + 1, rightDist)
                     done = True
                     return
-#                 elif 0 < frontDist and frontDist <= self.limit:
-# #                     self.map[x][y + 1] = 1
-                
-        #  check Right
+#                 elif frontDist < rightDist
+
+        print('\nRIGHT <> LEFT\n')
         if done != True:
-            print('Right FUNC')
             rightDist = self.SR.poll()
-            # leftDist = self.SL.poll()
+            leftDist = self.SL.poll()
             print('RightDist: ', rightDist)
+            print('LeftDist: ', leftDist)
             if rightDist != None:
-                if rightDist >= self.limit
+                #  check Right
+                if rightDist >= self.limit and rightDist >= leftDist + 100:
                     self.path.append([x, y, 'R', rightDist])
-                    print('Turning Right...')
+                    print('>>>>>>>>>>>> Turning Right & Moving Forward >>>>>>>>')
                     self.car.R()
                     self.car.F()
-                    print('\t\tand moving forward... (R)')
                     self.current = [x + 1, y]
     #                 self.counter += 1
     #                 self.map[x + 1][y] = self.counter
     #                 self.visited[x + 1][y] = True
-                    self.search(x + 1, y)
+                    self.search(x + 1, y, rightDist)
                     done = True
                     return
-#                 elif 0 < rightDist and rightDist <= self.limit:
-#     #                 self.map[x + 1][y] = 1
-        
-        #  check Left
-        if done != True:
-            print('Left FUNC')
-            leftDist = self.SF.poll()
-            print('LeftDist: ', leftDist)
-            if leftDist != None:
-                if leftDist > self.limit:
+                #  check Left
+                elif leftDist >= self.limit and leftDist >= rightDist:
                     self.path.append([x, y, 'L', rightDist])
-                    print('Turning Left...')
+                    print('<<<<<<<<<<<< Turning LEFT & Moving Forward <<<<<<<')
                     self.car.L()
                     self.car.F()
-                    print('\t\tand moving forward... (L)')
                     self.current = [x - 1, y]
-    #                 self.counter += 1
-    #                 self.map[x - 1][y] = self.counter
-    #                 self.visited[x - 1][y] = True
                     self.search(x - 1, y)
                     done = True
                     return
-#                 elif 0 < leftDist and leftDist <= self.limit:
-#     #                 self.map[x - 1][y] = 1
-        
+
         #  backtrack
         if done != True:
             print('backtracking...')
             self.car.L()
             self.car.L()
             self.backtrack()
+
+# #                 elif 0 < rightDist and rightDist <= self.limit:
+# #     #                 self.map[x + 1][y] = 1
+        
+#         #  check Left
+#         if done != True:
+#             print('Left FUNC')
+#             leftDist = self.SF.poll()
+#             print('LeftDist: ', leftDist)
+#             if leftDist != None:
+#                 if leftDist > self.limit:
+#                     self.path.append([x, y, 'L', rightDist])
+#                     print('Turning Left...')
+#                     self.car.L()
+#                     self.car.F()
+#                     print('\t\tand moving forward... (L)')
+#                     self.current = [x - 1, y]
+#     #                 self.counter += 1
+#     #                 self.map[x - 1][y] = self.counter
+#     #                 self.visited[x - 1][y] = True
+#                     self.search(x - 1, y)
+#                     done = True
+#                     return
+# #                 elif 0 < leftDist and leftDist <= self.limit:
+# #     #                 self.map[x - 1][y] = 1
+        
+
             
             
     
